7cea7ba472f3719e82ceaf02e7617791d959a730
Cotoc Vasile Gabriel <cotocvasilegabriel@yahoo.com>
2021-11-05 01:22:04 +0200

    Made Pagination bar component

--- src/Components/Pages/Collection/Collection.jsx
+++ src/Components/Pages/Collection/Collection.jsx
@@ -1,9 +1,46 @@
 import Header from 'Components/Common/Header/Header';
+import { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
+import PaginationBar from './PaginationBar/PaginationBar';
+
+const items = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24,
+];
+
+function Items({ currentItems }) {
+  return (
+    <div className="items">
+      {currentItems &&
+        currentItems.map((item) => (
+          <div>
+            <h3>Item #{item}</h3>
+          </div>
+        ))}
+    </div>
+  );
+}
 
 const Collection = () => {
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [pageNumber, setPageNumber] = useState(0);
+  const itemsPerPage = 6;
+
+  useEffect(() => {
+    const startOffset = (pageNumber * itemsPerPage) % items.length;
+    const endOffset = startOffset + itemsPerPage;
+
+    setCurrentItems(items.slice(startOffset, endOffset));
+
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [pageNumber, itemsPerPage]);
+
   return (
     <>
       <Header title={'COLLECTION'} navBarOnly={true} />
+      <Items currentItems={currentItems} />
+      <PaginationBar {...{ pageCount, setPageNumber }} />
     </>
   );
 };
